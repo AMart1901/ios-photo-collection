75e579aa4c17dcf34a16646c98b95e0c88af5f2b
Austin Martin <armartin2300@gmail.com>
2019-05-09 12:04:10 -0700

    General UI setup

---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Lambda_School_Loaner_64 on 5/9/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Lambda_School_Loaner_64 on 5/9/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
